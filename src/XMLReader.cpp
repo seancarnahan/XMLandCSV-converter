af7a4ee3bfe7e22bf4672d6b189aa12b9d6a2c3e
amkapasi <alykapasi@gmail.com>
2019-05-23 23:41:20 -0700

    current progress

--- src/XMLReader.cpp
+++ src/XMLReader.cpp
@@ -10,6 +10,17 @@ CXMLReader::~CXMLReader()
     XML_ParserFree(&parser);
 }
 
+void CXMLReader::StartElementHandler(void *data, const XML_Char *name, const XML_Char **atts)
+{
+    
+}
+
+void CXMLReader::EndElementHandler(void *data, const XML_Char *name)
+{
+    
+}
+
+void CXMLReader::CharacterHandler(
 
 bool CXMLReader::End()
 {
@@ -17,6 +28,18 @@ bool CXMLReader::End()
 }
 
 bool CXMLReader::ReadEntity(const SXMLEntity &entity)
+//from github.com/libexpat/libexpat/blob/master/expat/examples/outline.c "xml_parse"
 {
+    char *entityBuffer;
+    int len = (int)fread(entityBuffer, 0, input.end.tellg(), input);
+    XML_SetElementHandler(parser, start, end);
     
+    XML_SetCharacterHandler(parser, charhndl);
+    
+    if(XML_Parse(parser, entityBuffer, len, (int)input.eof()) == XML_STATUS_ERROR){
+        cerr << "Parse error @ line: " << XML_GetCurrentLineNumber(parser); << "\n" << XML_ErrorString(XML_GetErrorCode(parser));
+        return false;
+    }
+    
+    else return true;
 }
