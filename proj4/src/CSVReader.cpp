626b3cf9e66b9ca4eb3d48469419975f0ae7ff2f
seancarnahan <Vhellendoorn@ucdavis.edu>
2019-05-23 20:31:34 -0700

    cleanup

--- proj4/src/CSVReader.cpp
+++ proj4/src/CSVReader.cpp
@@ -2,37 +2,37 @@
 
 CCSVReader::CCSVReader(std::istream &in) //input(in)
 {
-  //csv_init(&Parser, 0);
+  csv_init(&Parser, 0);
 }
 
 CCSVReader::~CCSVReader()
 {
-  //csv_free(&Parser);
+  csv_free(&Parser);
 }
 
-// void CCSVReader::CallbackField(void *str, size_t len, void *data)
-// {
-//   //CCSVReader *Ptr = static_cast<CCSVReader>(data);
-//   //Ptr -> RowBuffer.push_back(std::string((char *)str, len));
-// }
+void CCSVReader::CallbackField(void *str, size_t len, void *data)
+{
+  CCSVReader *Ptr = static_cast<CCSVReader>(data);
+  Ptr -> RowBuffer.push_back(std::string((char *)str, len));
+}
 
-// void CCSVReader::CallbackRow(int c, void *data)
-// {
-//   //CCSVReader *Ptr = static_cast<CCSVReader>(data);
-// }
+void CCSVReader::CallbackRow(int c, void *data)
+{
+  CCSVReader *Ptr = static_cast<CCSVReader>(data);
+}
 
 bool CCSVReader::End() const
 {
-  //return in.eof();
-  return true;
+  return in.eof();
+  
 }
 
 bool CCSVReader::ReadRow(std::vector<std::string> &row)
 {
-    // std::stringstream inCopy = in;
-    // inCopy.seekg(0, inCopy.end);
-    // int nBytes = inCopy.tellg();
-    // csv_parse(Parser, row, nBytes, CallbackField(), CallbackRow()/*c*/, in, in);
-    // csv_fini(Parser, CallbackField(), CallbackRow(/*c*/, in), in);
+    std::stringstream inCopy = in;
+    inCopy.seekg(0, inCopy.end);
+    int nBytes = inCopy.tellg();
+    csv_parse(Parser, row, nBytes, CallbackField(), CallbackRow()/*c*/, in, in);
+    csv_fini(Parser, CallbackField(), CallbackRow(/*c*/, in), in);
   return true;
 }
